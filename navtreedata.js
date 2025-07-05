789acedb53646363a864e00c849f5fd037ac58b9
i2git <i2git@users.noreply.github.com>
2025-07-05 08:30:30 +0000

    deploy: moves-rwth/storm@c7448a2ee1c4db5d4bd892995768fa790247511b

---
+++ navtreedata.js
@@ -0,0 +1,224 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Storm", "index.html", [
+    [ "Storm - A Modern Probabilistic Model Checker", "index.html", "index" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/d1d/classstorm_1_1modelchecker_1_1exploration__detail_1_1_exploration_information.html#a788ac11ef96389681b42c3c573333009",
+"d0/d44/classstorm_1_1automata_1_1_h_o_a_consumer_d_a.html#a3694b8f448cef8f993d9dbe3a368659a",
+"d0/d5a/classstorm_1_1gbar_1_1modelchecker_1_1_abstract_abstraction_refinement_model_checker.html#a49ebf2053bf792f6abfb40ee469f8019",
+"d0/d73/_optimistic_value_iteration_helper_8h.html",
+"d0/d7d/classstorm_1_1storage_1_1geometry_1_1_polytope_tree.html#ace17d0a5637ddcc4f08a7f4165b6838e",
+"d0/d89/_type_8h.html#a8fa4eaeb359d7a207e03cfa85aabac25",
+"d0/dba/_sparse_item_labeling_parser_test_8cpp_source.html",
+"d0/dd7/classstorm_1_1dd_1_1_add.html#a4a07fdfb81424a0a877f05de785bf096",
+"d0/ddc/classstorm_1_1settings_1_1modules_1_1_general_settings.html#ab7880f07f548a6e2ef42768af1e184bb",
+"d0/df2/classstorm_1_1modelchecker_1_1_sparse_dtmc_elimination_model_checker.html#a44c2823d8d7e2a2907b52e7bb640b78a",
+"d1/d00/classstorm_1_1dft_1_1settings_1_1modules_1_1_fault_tree_settings.html#af011dc87599501525630e08de79ae42c",
+"d1/d2b/classstorm_1_1jani_1_1_composition_information.html#a0627dda15f098da611394516a62063d0",
+"d1/d43/classstorm_1_1settings_1_1modules_1_1_belief_exploration_settings.html#a2b105ce52214bddc4eabf760bb65d46a",
+"d1/d5f/classstorm_1_1pars_1_1_feasibility_synthesis_task.html#a606a42cd6f976e5d8412d706c4965fdb",
+"d1/d88/classstorm_1_1modelchecker_1_1_sparse_parameter_lifting_model_checker.html#a17f40f1f5cf7faf5ab7dcf37e82484d9",
+"d1/db4/classstorm_1_1jani_1_1detail_1_1_array_expression_elimination_visitor_1_1_result_type.html#a4a674ef7cb0f326735d04a6aa1dd6a94",
+"d1/dd0/classstorm_1_1modelchecker_1_1_sparse_mdp_prctl_model_checker.html#a38a297b90c4ad0a4821e3fe0a4a3fd5b",
+"d1/df1/classstorm_1_1solver_1_1helper_1_1_g_s_v_i_backend.html",
+"d2/d18/classstorm_1_1pomdp_1_1_iterative_policy_search_1_1_statistics.html#aed62c97a48cc6efae9e3338c6e4f1711",
+"d2/d2f/_jani_local_eliminator_tests_8cpp.html#abd0317977db68a17d3557e14eda67453",
+"d2/d53/classstorm_1_1logic_1_1_to_prefix_string_visitor.html#a9c14ad0e809e514b75906ce99a6c4579",
+"d2/d6f/_symbolic_qualitative_game_result_8cpp_source.html",
+"d2/d81/classstorm_1_1storage_1_1_parameter_region.html#ada93cc4d10d4bb57fbe5545809acbac7",
+"d2/da1/classstorm_1_1logic_1_1_unary_path_formula.html#a47f7f2b4fcd0712ea157c825e892e3fa",
+"d2/db2/classstorm_1_1jani_1_1_filter_expression.html#ae537ce933ec0cb334ffaff7af6c82722",
+"d2/dd2/classstorm_1_1dd_1_1bisimulation_1_1_internal_sylvan_signature_refiner_base.html#aa55f3c0853f80cbd3db556aec0083436",
+"d2/df0/classstorm_1_1settings_1_1modules_1_1_conversion_general_settings.html#a5f9787d75137fe41b58055d627c476b8",
+"d2/dff/structstorm_1_1modelchecker_1_1helper_1_1rewardbounded_1_1_dimension.html#a6abdb92b827095c3336cabd83f0a1f99",
+"d3/d15/structstorm_1_1utility_1_1graph_1_1_symbolic_game_prob01_result.html#a9700e2272100db48d5e083b59f46456a",
+"d3/d23/classstorm_1_1expressions_1_1_integer_literal_expression.html",
+"d3/d47/classstorm_1_1solver_1_1_acyclic_linear_equation_solver.html#ac218ee17e6b4a8dd5a564fae54965cae",
+"d3/d5f/structstorm_1_1utility_1_1graph_1_1_explicit_game_prob01_result.html#ac68525a908ee074f6c3e87818f851c88",
+"d3/d78/classstorm_1_1logic_1_1_conditional_formula.html#a244cf35814069dc5c52538509cb1abd3",
+"d3/d8c/classstorm_1_1settings_1_1modules_1_1_build_settings.html#ae38bf39f3900fc85a4df4de3da72ba18",
+"d3/db3/classstorm_1_1settings_1_1modules_1_1_conversion_input_settings.html#a07bb44eb75818dfa6a7dcf0f2ed4a45f",
+"d3/de2/_prism_export_settings_8cpp_source.html",
+"d4/d0d/structstorm_1_1converter_1_1_jani_conversion_options.html",
+"d4/d40/classstorm_1_1dd_1_1_internal_add.html#a54a050eafd38880b1efff5fe070772f3",
+"d4/d63/classstorm_1_1modelchecker_1_1helper_1_1_single_value_model_checker_helper.html#a217cff1f403d573696695ca25817e44f",
+"d4/d6a/classstorm_1_1logic_1_1_formula.html#aaff2912e190368bb1b994e9dc7f5b066",
+"d4/d7a/classstorm_1_1_ovi_solver_environment.html#a8955d6d401fe2f739b03f4ebdf433633",
+"d4/d99/classstorm_1_1expressions_1_1_unary_expression.html#a020df26e88ea9402cd9335f62457fc05",
+"d4/dc4/classstorm_1_1modelchecker_1_1helper_1_1_sparse_ctmc_csl_helper.html#a72a07daab1b7a025c780b278a449de01",
+"d4/de8/classstorm_1_1solver_1_1_native_linear_equation_solver.html#af4a8c426cae7f2342fb278317a69b552",
+"d5/d01/classstorm_1_1storage_1_1_belief_manager.html#a43f99279c0d45a94c2b5ba3b8976972f",
+"d5/d1c/structstorm_1_1dft_1_1storage_1_1_b_e_colour_class.html#aa919ea06ed5a6de1031b2cf38f28bfd0",
+"d5/d36/classstorm_1_1solver_1_1helper_1_1_s_v_i_backend.html#a73dd5a683df5ff015cf769081c6b9c57",
+"d5/d65/classstorm_1_1modelchecker_1_1_explicit_model_checker_hint.html#a45aac0beadba529087607111e5ada478",
+"d5/d86/classstorm_1_1dft_1_1storage_1_1elements_1_1_d_f_t_spare.html#a85fdc801c187dda245bc8a4e18c504cd",
+"d5/dad/classstorm_1_1transformer_1_1_make_state_set_observation_closed.html#adddfd93350dfcdace802a0c5ba15aea4",
+"d5/dd0/classstorm_1_1storage_1_1_sparse_matrix_1_1rows.html",
+"d5/df2/_transcendental_number_literal_expression_8cpp_source.html",
+"d6/d0a/classstorm_1_1jani_1_1_automaton.html#a0774f91c0553dcd3dd28a6b121a3114f",
+"d6/d0c/_exploration_order_8h.html#ae7029986a9c3109786d6a9a26f810490aa869584a06e3efb1c723f07b6edc4cea",
+"d6/d2c/classstorm_1_1modelchecker_1_1_sparse_dtmc_instantiation_model_checker.html#afe3beb707f2d469a161ebd7c5827096b",
+"d6/d4a/_symbolic_mdp_prctl_helper_8cpp.html#a39aa1557060a99a9de537e01e318eabd",
+"d6/d66/classstorm_1_1models_1_1symbolic_1_1_mdp.html#a09e94ede1d5b5af6c2550b13fd933efe",
+"d6/d79/classstorm_1_1builder_1_1_belief_mdp_explorer.html#a5200be8cab04c13366a1e9a55ee49105",
+"d6/d80/constants_8cpp.html#abb3f5c53fff6c5fe5ebbbcc549147c18",
+"d6/d98/classstorm_1_1dft_1_1storage_1_1elements_1_1_d_f_t_element.html#af5172d37ab23f606fe3b345d7c4ac76d",
+"d6/dad/classstorm_1_1settings_1_1modules_1_1_jani_export_settings.html#aa16b938834eee3a78759b397f4529a19",
+"d6/dbf/classstorm_1_1logic_1_1_reward_model_name_substitution_visitor.html#af1f9d8f102e6e6fc174fc371e7cc0bdf",
+"d6/de9/classstorm_1_1storage_1_1geometry_1_1_polytope.html#a4a1d447845e806c7cfa36d29defcf466",
+"d6/df3/graph_8h.html#a686bbac8b7fb31969f5a31ba211bf3a1",
+"d7/d0e/classstorm_1_1dd_1_1_internal_add_3_01_dd_type_1_1_sylvan_00_01_value_type_01_4.html#a848dffd50c8a4b2691cf87778aed31c5",
+"d7/d25/namespacestorm_1_1modelchecker_1_1helper_1_1lexicographic_1_1spothelper.html#a1afa35bee6c1d238de115551a528635eab6036f685a1ca973f96c6691c1a3a472",
+"d7/d3d/classstorm_1_1jani_1_1_json_exporter.html#aa2830a619dd283d850c236b9d1f6910e",
+"d7/d69/classstorm_1_1dft_1_1storage_1_1_failable_elements_1_1const__iterator.html#a611112debb57487b5f62f6dc7e7eccab",
+"d7/d8d/classstorm_1_1expressions_1_1_reduce_nesting_visitor.html#a6122c3879264cad95bf388c0119ee979",
+"d7/da4/_sylvan_dd_test_8cpp.html#a812f8dc4a09339f94421b6d3f6e86e4f",
+"d7/db8/classstorm_1_1pomdp_1_1modelchecker_1_1_preprocessing_pomdp_value_bounds_model_checker.html#a0113639753ec5b73b59eaa0f6c56d025",
+"d7/dd6/classstorm_1_1pomdp_1_1_memless_search_options.html",
+"d7/df1/classstorm_1_1utility_1_1_extremum.html#a616052e99314f35dcd95aa8b3c82df09",
+"d8/d11/classstorm_1_1expressions_1_1_expression.html#a2ea12bb555c1a3ff309a23032145dc16",
+"d8/d24/classstorm_1_1settings_1_1modules_1_1_abstraction_settings.html#a2fe6abd6f9b2e59134adfacb3edd509e",
+"d8/d43/namespacestorm_1_1expressions.html#a44b94b986bea9e69602d481f18c46883",
+"d8/d54/_argument_type_inferation_helper_8cpp.html#aa19b656d9b17336f03dcbc7aac832a9c",
+"d8/d6c/classstorm_1_1jani_1_1detail_1_1_function_eliminator_traverser.html#a0db7f5dbaedef2608f80a4cc49b69873",
+"d8/d89/classstorm_1_1gbar_1_1abstraction_1_1_symbolic_quantitative_game_result_min_max.html",
+"d8/d9a/classstorm_1_1builder_1_1_builder_options.html#a853afec749baadf89f6e30ec689506e4",
+"d8/dad/classstorm_1_1logic_1_1_formula_visitor.html#a05e8d5c302a593f194b6abbdd0376fa1",
+"d8/dcb/_gspn_parser_8h.html",
+"d8/de6/structstorm_1_1storage_1_1_bisimulation_decomposition_1_1_options.html#af61495bdd5393289e99a699c814aa74a",
+"d9/d04/_parametric_mode_8h.html#a7e68631c4fd37989dfe5a4c98d372cbc",
+"d9/d33/classstorm_1_1logic_1_1_bounded_until_formula.html#a046a2f3129217432af9932e6d04aa042",
+"d9/d43/classstorm_1_1prism_1_1_renaming_composition.html#a1b720713f65327656a243716152d9630",
+"d9/d68/classstorm_1_1dd_1_1_internal_dd_manager_3_01_dd_type_1_1_c_u_d_d_01_4.html",
+"d9/d7a/classstorm_1_1dft_1_1adapters_1_1_s_f_t_b_d_d_property_formula_adapter.html",
+"d9/d85/classstorm_1_1logic_1_1_cumulative_reward_formula.html#a72bdcb73b7d0344005d0099d458e3e41",
+"d9/db0/storm-conv_2api_2storm-conv_8cpp.html#a9be419df4f06c3001e9e1bc0cd71173a",
+"d9/dc0/classstorm_1_1dd_1_1_internal_add_3_01_dd_type_1_1_c_u_d_d_00_01_value_type_01_4.html#aa7dde7e75129da8334f3b6730ac799f8",
+"d9/dcf/classstorm_1_1dft_1_1storage_1_1_d_f_t_state.html#a5a50ed6096ebda21a91096439848b1e8",
+"d9/df9/classstorm_1_1expressions_1_1_binary_relation_expression.html#ad6d8b484e865a46ffeab0cd0326d2e88",
+"da/d3a/classstorm_1_1adapters_1_1_smt2_expression_adapter.html#a52c2705a1d79be790f53cf2c94f67baf",
+"da/d5d/_d_f_t_pand_8h.html",
+"da/d75/_sparse_matrix_8cpp.html#a3049ee7d4f88cba8e903947b7beef2c7",
+"da/d89/classstorm_1_1utility_1_1solver_1_1_soplex_lp_solver_factory.html#af1dcc0a27bb7c2f6fb8fa426f32536f4",
+"da/dbb/classstorm_1_1solver_1_1_min_max_linear_equation_solver.html#ad1c9c47a1baa85fdd01bdf8ad26c8ca9",
+"da/ddb/classstorm_1_1generator_1_1_observation_dense_belief_state.html#ab69e321b4fd169e7cb9cbed654b8019f",
+"da/deb/_build_settings_8cpp.html#a0bdfd3d4544c85a3fe0ef83595673e4b",
+"db/d03/_bit_vector_test_8cpp.html#a0e54a595273012b207e77889a3bd78f2",
+"db/d18/classstorm_1_1expressions_1_1_simple_valuation.html#aecfac6fbcf7937effbbf3c697e8d0027",
+"db/d3c/classstorm_1_1logic_1_1_fragment_checker.html#a8aa8231c485da243becaa04dfba41069",
+"db/d51/_jani_model_test_8cpp_source.html",
+"db/d63/classstorm_1_1expressions_1_1_variable_set_predicate_splitter.html#aa7057248454dde8fd81edc80dc568f6d",
+"db/d70/classstorm_1_1prism_1_1_program.html#ab7b94ca6e233dae5ffa1a9f89d285ca6",
+"db/d80/classstorm_1_1settings_1_1_argument_validator_factory.html#ac3887a4902470d3554a03835db634bdc",
+"db/d9e/classstorm_1_1modelchecker_1_1multiobjective_1_1_deterministic_scheds_objective_helper.html",
+"db/db6/classstorm_1_1modelchecker_1_1multiobjective_1_1_standard_pcaa_weight_vector_checker.html#ab01a89b7a4591b050e006175f51f62c4",
+"db/dc8/classstorm_1_1pomdp_1_1_winning_region.html#afe00041450d5136da29dd4ba2d3c44cd",
+"db/de3/classstorm_1_1logic_1_1_game_formula.html#a7362972b6644d2f9b98f6a66eb0b6df7",
+"dc/d05/structstorm_1_1pomdp_1_1modelchecker_1_1_belief_exploration_pomdp_model_checker_options.html#ab406e460efa922182d522de2d2003926",
+"dc/d1e/classstorm_1_1models_1_1sparse_1_1_smg.html",
+"dc/d3f/classstorm_1_1utility_1_1solver_1_1_z3_lp_solver_factory.html#a4a36e7ac12cb66f21dbacd96a939a6fe",
+"dc/d4f/classstorm_1_1parser_1_1_expression_creator.html#aef37c2d58758f8b1791ca83b166bc891",
+"dc/d70/_belief_exploration_pomdp_model_checker_8cpp.html#af8d15e34b4e4a5760bdf9af8611c7d15",
+"dc/d85/classstorm_1_1jani_1_1_variable_set.html#a51016104ff857b76c957bbf0b92d0eb6",
+"dc/da7/classstorm_1_1gbar_1_1abstraction_1_1_abstraction_information.html#a24f9c982425a7e9884a94562898c6fff",
+"dc/dc2/classstorm_1_1modelchecker_1_1multiobjective_1_1_visiting_times_helper.html",
+"dc/ddc/classstorm_1_1jani_1_1_reward_model_information.html#a35217a93534d3fe1237e3bdb5252b5b9",
+"dc/ded/classstorm_1_1logic_1_1_fragment_specification.html#a478e4738e9e309714a98cb8bca384c00",
+"dc/df0/classstorm_1_1storage_1_1_sparse_matrix.html#a4a5d7ebde77e5b23bf82900311efd12a",
+"dc/df9/classstorm_1_1dd_1_1_internal_bdd_3_01_dd_type_1_1_c_u_d_d_01_4.html#a29859c03faae7eaed42745f45c4bc6dd",
+"dd/d17/classstorm_1_1builder_1_1_module_composer.html#a193e7dc5e10f6ec7de3aad48e898e5b9",
+"dd/d22/classstorm_1_1jani_1_1_model.html#a0dbfd3bad95f9c30510543acdbe86c33",
+"dd/d29/classstorm_1_1models_1_1sparse_1_1_item_labeling.html#a387e22ceff55479da8acd71d3f4f26b6",
+"dd/d45/classstorm_1_1models_1_1symbolic_1_1_stochastic_two_player_game.html#a690f9023faaebfcf26dee32fe782190b",
+"dd/d65/classstorm_1_1parser_1_1_formula_parser.html#a05060f3a21aa6dcbb75f811129a18502",
+"dd/d89/classstorm_1_1storage_1_1_consecutive_uint64_dynamic_priority_queue.html#a8e9500eb589003fbc1937dbdfed7a55b",
+"dd/d96/storm_2api_2export_8h.html",
+"dd/db8/classstorm_1_1expressions_1_1_value_array_expression.html#a714c25dd51746347b7abc84ed138539b",
+"dd/dc7/permutation_8cpp.html#abe20bc3377f68e5b30ae68303b169017",
+"dd/de4/structstorm_1_1builder_1_1_combined_edges_system_composer_1_1_action_dd.html#a4a402c2cc7fdd3f7292c18d7cf91443a",
+"de/d1e/storm-dft_8h.html#a3c0a1a05b31f736f7a9e80a7029bd9dc",
+"de/d35/classstorm_1_1settings_1_1modules_1_1_multi_objective_settings.html#ac11e96966c0c7cc5d2df5f7e5dbbc3b3",
+"de/d65/classstorm_1_1expressions_1_1_array_expression.html#a295d928a12639db0fdc51cbf0e074e0e",
+"de/d88/_terminal_states_getter_8h_source.html",
+"de/daa/structstorm_1_1generator_1_1_integer_variable_information.html#a3290d0537836d83fe221046127048387",
+"de/dc4/classstorm_1_1gbar_1_1abstraction_1_1jani_1_1_automaton_abstractor.html#a0b3fcbc2bde314575e620a1e9bd89e53",
+"de/dd5/classstorm_1_1settings_1_1modules_1_1_g_s_p_n_settings.html#a61b63aedb098e7aad453c3bf641e8fcb",
+"de/df6/classstorm_1_1gbar_1_1abstraction_1_1prism_1_1_command_abstractor.html#a371c117c91bb9fb3666765ee2d88f113",
+"df/d19/classstorm_1_1transformer_1_1_symbolic_ctmc_to_sparse_ctmc_transformer.html#a0c5ac46edb8590ac401427dd7ca41b61",
+"df/d48/classstorm_1_1storage_1_1_matrix_entry.html",
+"df/d60/classstorm_1_1jani_1_1_edge.html#a5ba6903bfee3a99edc042d4cd92ecbf6",
+"df/d73/structstorm_1_1transformer_1_1_binary_pomdp_transformer_row_group.html#a834eec5fb9e3176cfe6a1d7c4377d7e5",
+"df/d8e/_fragment_specification_8cpp.html#a731d3f83996600bef6d672c26c0a2bc4",
+"df/da5/structstorm_1_1generator_1_1_choice.html#affb769b9cc031eb672ee8bb8402273e9",
+"df/dc4/structstorm_1_1storage_1_1_strongly_connected_component_decomposition_options.html#af8513aefb74bd35551f02f13950993d1",
+"df/ddc/namespacestorm_1_1solver.html#a67886b4c680b38c035829a3155d8418f",
+"df/df5/classstorm_1_1jani_1_1_expression_to_json.html#a4a525ba4a53da2a3f1c42d545b38a9ad",
+"dir_f49d830f0df1e6b69da9d8d965cbeb8c.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
